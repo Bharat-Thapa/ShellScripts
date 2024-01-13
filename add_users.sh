957f93a3f472d6f9f1969bc5cd2f689abbc4190f
Bharat-Thapa <btpa2000115@gmail.com>
2024-01-13 10:56:52 +0000

    new file added

---
+++ add_users.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for ((i=1; i<=3; i++))
+do	
+read -p "Enter username " username
+sudo useradd -m $username
+done
+
+echo "users added successfully"
+
+sudo cat /etc/passwd | grep user*
+
