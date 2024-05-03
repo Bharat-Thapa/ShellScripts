0efa860914710ef02f0e98df13b4e0546c3dfabe
Bharat Thapa <80249581+Bharat-Thapa@users.noreply.github.com>
2024-05-04 00:30:06 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo apt-get update
 
-sudo apt-get install docker.io docker-compose -y
+sudo apt-get install docker.io -y
 
 sudo usermod -aG docker $USER
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
